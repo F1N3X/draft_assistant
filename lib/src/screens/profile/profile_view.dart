0ae092ebbb5ab8135f3b4dd8e4f38efb9337bc11
F1N3X <quartzman92@gmail.com>
2026-09-20 15:50:57 +0200

    refactor: moving google auth logic in services directory

--- lib/src/screens/profile/profile_view.dart
+++ lib/src/screens/profile/profile_view.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:draft_assistant/src/widgets/app_bar.dart';
 import 'package:draft_assistant/src/widgets/bottom_navigation_bar.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:sign_in_button/sign_in_button.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:google_sign_in_platform_interface/google_sign_in_platform_interface.dart';
-import 'package:draft_assistant/src/services/firestore_service.dart';
+import 'package:draft_assistant/src/services/google_auth.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
@@ -22,42 +19,6 @@ class ProfileView extends StatelessWidget {
         ],)
       ),
       bottomNavigationBar: draftAssistantBottomNavigationBar(context, 2)
-    );
-  }
-}
-
-Future<void> ensureIintialized () {
-  return GoogleSignInPlatform.instance.init(const InitParameters());
-}
-
-Future<void> signInWithGoogle(BuildContext context) async {
-  try {
-    await ensureIintialized();
-
-    final AuthenticationResults result = await GoogleSignInPlatform.instance.authenticate(const AuthenticateParameters()); 
-
-    final String? idToken = result.authenticationTokens.idToken ;
-
-    if (idToken != null) {
-      final OAuthCredential credential = GoogleAuthProvider.credential(idToken: idToken);
-
-      UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
-      final firebaseUser = userCredential.user;
-
-      if (firebaseUser != null) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Connexion réussie avec Google : ${firebaseUser.displayName}')),
-        );
-      }
-    }
-
-  } on GoogleSignInException catch (e) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Erreur lors de la récupération du token Google : $e')),
-    );
-  } on FirebaseAuthException catch (e) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Erreur lors de la connexion avec Firebase: $e')),
     );
   }
 }
