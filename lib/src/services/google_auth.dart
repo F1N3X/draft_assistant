d73737ef8fc384221425716b331038bb82b48e1a
F1N3X <quartzman92@gmail.com>
2026-09-20 16:33:36 +0200

    fix: using modern google_sign_in syntax

--- lib/src/services/google_auth.dart
+++ lib/src/services/google_auth.dart
@@ -1,32 +1,35 @@
-import 'package:google_sign_in_platform_interface/google_sign_in_platform_interface.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-Future<void> ensureIintialized () {
-  return GoogleSignInPlatform.instance.init(const InitParameters());
-}
-
 Future<void> signInWithGoogle(BuildContext context) async {
   try {
-    await ensureIintialized();
+    const String serverClientId = String.fromEnvironment('GOOGLE_CLIENT_ID');
 
-    final AuthenticationResults result = await GoogleSignInPlatform.instance.authenticate(const AuthenticateParameters()); 
-
-    final String? idToken = result.authenticationTokens.idToken ;
-
-    if (idToken != null) {
-      final OAuthCredential credential = GoogleAuthProvider.credential(idToken: idToken);
-
-      UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
-      final firebaseUser = userCredential.user;
-
-      if (firebaseUser != null) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Connexion réussie avec Google : ${firebaseUser.displayName}')),
-        );
-      }
+    if (serverClientId.isEmpty) {
+      throw Exception('La variable GOOGLE_CLIENT_ID est introuvable.');
     }
 
+    await GoogleSignIn.instance.initialize(
+      serverClientId: serverClientId,
+    );
+
+    final GoogleSignInAccount? googleUser = await GoogleSignIn.instance.authenticate();
+    
+    if (googleUser == null) return;
+
+    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+    final OAuthCredential credential = GoogleAuthProvider.credential(idToken: googleAuth.idToken);
+
+    final UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+    final firebaseUser = userCredential.user;
+
+    if (firebaseUser != null && context.mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Connexion avec Google réussie : ${firebaseUser.displayName}')),
+      );
+    }
   } on GoogleSignInException catch (e) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(content: Text('Erreur lors de la récupération du token Google : $e')),
